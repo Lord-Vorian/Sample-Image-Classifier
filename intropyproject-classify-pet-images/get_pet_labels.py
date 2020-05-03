4ebc26abad0d3fb2b57ed7296fe64b9a15daac1f
lord-vorian <highskieseverywhere@gmail.com>
2020-05-02 20:39:02 -0600

    fix image_dir

--- intropyproject-classify-pet-images/get_pet_labels.py
+++ intropyproject-classify-pet-images/get_pet_labels.py
@@ -43,5 +43,5 @@ def get_pet_labels(image_dir):
     """
     # Replace None with the results_dic dictionary that you created with this
     # function
-    results_dic = {filename: [" ".join(filename.lower().split('_')[:-1])] for filename in image_dir}
+    results_dic = {filename: [" ".join(filename.lower().split('_')[:-1])] for filename in listdir(image_dir)}
     return results_dic
